c2c81168e80d213fbd8d312bf442162cb96c32e0
Conboy-S <conboy99@googlemail.com>
2021-08-06 12:43:14 +0100

    third commit, modified function

--- message-generator.js
+++ message-generator.js
@@ -4,7 +4,7 @@ var quotes = [
   'Try again... or you can choose to open the mystery box!',
 ]
 
-const messageGenerator() {
+function messageGenerator() {
   var randomNumber = Math.floor(Math.random() * (quotes.length));
   console.log(quotes[randomNumber]);
-};   
+};
