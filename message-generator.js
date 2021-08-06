39c11bfc1a793907a9da0a1c404baa12cb6a7d3b
Conboy-S <conboy99@googlemail.com>
2021-08-06 12:40:01 +0100

    Second commit- initial function pre testing

--- message-generator.js
+++ message-generator.js
@@ -1,1 +1,10 @@
-const messageGenerator   
+var quotes = [
+  'You are a winner!',
+  'You are a loser...',
+  'Try again... or you can choose to open the mystery box!',
+]
+
+const messageGenerator() {
+  var randomNumber = Math.floor(Math.random() * (quotes.length));
+  console.log(quotes[randomNumber]);
+};   
